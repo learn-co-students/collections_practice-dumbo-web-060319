c80f92b81460fe9cd2be193e15c47a58f03a847c
Alexander Schelchere <ascellie@gmail.com>
2019-06-04 14:56:40 -0400

    swap elements - complete

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,3 +11,16 @@ end
 def sort_array_char_count(arr)
     arr.sort{ |word1, word2| word1.length <=> word2.length }
 end
+
+def swap_elements(arr)
+    second_place = arr[1]
+    third_place = arr[2]
+
+    arr[1] = third_place
+    arr[2] = second_place
+    # binding.pry
+    puts arr
+    arr
+end
+
+# element_swap(["a", "b", "c"])
